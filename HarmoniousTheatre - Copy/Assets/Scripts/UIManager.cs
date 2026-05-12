0563adb25942bed5ef73868aa0dc8bb93a6d963b
inewndo <eliz.shchedrina@gmail.com>
2026-05-12 03:05:22 -0400

    pause menu

--- HarmoniousTheatre - Copy/Assets/Scripts/UIManager.cs
+++ HarmoniousTheatre - Copy/Assets/Scripts/UIManager.cs
@@ -4,7 +4,17 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    //public GameObject tutPannel;
+    public GameObject pauseMenuUI;
+    public static bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused) ResumeGame();
+            else PauseGame();
+        }
+    }
     public void Play()
     {
         SceneManager.LoadScene("Present");
@@ -13,8 +23,29 @@ public class UIManager : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0f; // Freeze gameplay
+        AudioListener.pause = true; // Pause all audio
+        isPaused = true;
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
-   
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; // Resume gameplay
+        AudioListener.pause = false;
+        isPaused = false;
+    }
+
+
 
     public void Exit()
     {
